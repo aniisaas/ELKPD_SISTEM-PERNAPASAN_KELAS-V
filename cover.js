164df6ee9a338508250a1c75e46d1bdcec6a81f3
aniisaas <sitianisa1204@gmail.com>
2025-06-13 16:30:02 +0700

    Add files via upload

---
+++ cover.js
@@ -0,0 +1,18 @@
+DesktopResponsive={"1009":{
+"pageLayer":{"w":1009,"h":662,"bgColor":"#ffffff"}
+,
+"button311":{"x":384,"y":376,"w":37.000000,"h":35.000000,"stylemods":[{"sel":"div.button311Text","decl":" { position:fixed; left:5px; top:3px; width:27px; height:29px;}"},{"sel":"span.button311Text","decl":" { display:table-cell; position:relative; width:27px; height:29px; vertical-align:middle; text-align:center; line-height:7px; font-size:7px; font-family:\"Lucida Sans Unicode\"; color:#ffffff;}"}],"bOffBottom":0  ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACUAAAAjCAYAAAATx8MeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAB7SURBVFhH7dHBDYAgDIVh9nEVRnEQB3FOal9CE/SgHKoH838JKY8IbWIBAAD4ITPb+nZKa23v2/dch1JTPzOva+RYkVV1T9+pKqcaH/WG1dc4zCnf1VQzQ/lalONcNe5FTqVH9Q8kmvf94+8b86c0oHjz2o8AAABSlXIAUTqyrVsHTggAAAAASUVORK5CYII=" ,"tdO": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACUAAAAjCAYAAAATx8MeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAB7SURBVFhH7dHBDYAgDIVh9nEVRnEQB3FOal9CE/SgHKoH838JKY8IbWIBAAD4ITPb+nZKa23v2/dch1JTPzOva+RYkVV1T9+pKqcaH/WG1dc4zCnf1VQzQ/lalONcNe5FTqVH9Q8kmvf94+8b86c0oHjz2o8AAABSlXIAUTqyrVsHTggAAAAASUVORK5CYII=" ,"tdD": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACYAAAAkCAYAAADl9UilAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAB6SURBVFhH7dHbDYAgDIVh9nEVRnEQB3FOak/SJuiD8IA+/V9CyiFcmlAAAABwY2ZHTKe01s6YfuvZmB72NfO6Z86RWVXntE9Vebn+Yn+0+ugbuuW3utxMYz425VxXzXOZl9PF+g/JBmI+/Mo+/05NijdQYwkAAAAjpVykMLKtXSINnwAAAABJRU5ErkJggg==" ,"tdDi": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACUAAAAjCAYAAAATx8MeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAB7SURBVFhH7dHBDYAgDIVh9nEVRnEQB3FOal9CE/SgHKoH838JKY8IbWIBAAD4ITPb+nZKa23v2/dch1JTPzOva+RYkVV1T9+pKqcaH/WG1dc4zCnf1VQzQ/lalONcNe5FTqVH9Q8kmvf94+8b86c0oHjz2o8AAABSlXIAUTqyrVsHTggAAAAASUVORK5CYII="  ,"fd": "" ,"fdO": "" ,"fdD": "" ,"fdDi": "" ,"p": "M 9.000000 1.000000 L 28.000000 1.000000 L 31.125000 1.625000 L 33.687500 3.375000 L 35.375000 5.875000 L 36.000000 9.000000 L 36.000000 26.000000 L 35.375000 29.125000 L 33.687500 31.687500 L 31.125000 33.375000 L 28.000000 34.000000 L 9.000000 34.000000 L 6.000000 33.437500 L 3.375000 31.687500 L 1.625000 29.062500 L 1.000000 26.000000 L 1.000000 9.000000 L 1.625000 5.875000 L 3.375000 3.375000 L 5.875000 1.625000 L 9.000000 1.000000 z","i":"images/button311.png","irol":"images/button311_over.png","ion":"images/button311_down.png","idis":"images/button311_disabled.png"}
+,
+"image42":{"x":270,"y":0,"w":468,"h":662,"bOffBottom":0,"i":"images/1.png"}
+,
+"entry241":{"x":421,"y":498,"w":150,"h":24,"fsize":16,"bOffBottom":0}
+,
+"entry271":{"x":421,"y":498,"w":150,"h":24,"fsize":16,"bOffBottom":0}
+,
+"RCDResetQuestion":function(){
+try{if(window.dragMgr)window.dragMgr.clearDropZones();}catch(e){if(e&&e.message)console.log(e.message);}
+}
+,
+"RCDResultResize":function(){}
+,"preload":['images/1.png','images/button311.png','images/button311_over.png','images/button311_down.png','images/button311_disabled.png']
+}}
